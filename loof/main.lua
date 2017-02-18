aa24e16657d9c7ede4c7181e382899b4de693ca0
fab <fdev31@gmail.com>
2017-02-18 17:39:52 +0100

    remove unused debug module

--- loof/main.lua
+++ loof/main.lua
@@ -1,8 +1,6 @@
---debugWorldDraw = require("debugWorldDraw")
 loof = require('game')
 objects = require('objects')
 cfg = require('config')
-
 
 function love.load()
     game = loof.Game:clone():init()
@@ -15,7 +13,4 @@ end
 
 function love.draw()
     game:draw()
-    if cfg.DEBUG and debugWorldDraw ~= nil then
-        debugWorldDraw(game.board.world,0,0,game.board.size, game.board.size)
-    end
 end

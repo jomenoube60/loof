84e5427a133007e4601f3283bb5ceac90ec889a8
fab <fdev31@gmail.com>
2017-02-22 22:51:34 +0100

    manage self contained packages

--- package.sh
+++ package.sh
@@ -3,3 +3,14 @@ cd loof
 find . -name "*.orig" -exec rm {} \;
 find . -name "*.sw?" -exec rm {} \;
 zip -9r ../loof.love .
+
+cd ..
+cat /bin/love loof.love > loof_lnx
+chmod +x loof_lnx
+
+mkdir loof_win32
+cp -r love*win*/*.dll loof_win32/
+cat love*win*/love.exe loof.love > loof_win32/loof.exe
+
+zip -9r loof-win32.zip loof_win32
+rm -fr loof_win32

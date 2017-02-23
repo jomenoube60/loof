989125f6cb35f1c3bdec45872f0e643e792d5f70
fab <fdev31@gmail.com>
2017-02-23 18:35:29 +0100

    add OSX support

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ rm -fr *.zip
 cd loof
 find . -name "*.orig" -exec rm {} \;
 find . -name "*.sw?" -exec rm {} \;
-zip -9r ../loof.love .
+zip -9ry ../loof.love .
 
 cd ..
 cat /bin/love loof.love > loof_lnx
@@ -17,7 +17,7 @@ D="loof-$pkg"
 mkdir $D
 cp -r love*win*/*.dll $D
 cat love*win*/love.exe loof.love > $D/loof.exe
-zip -9r $D.zip $D
+zip -9ry $D.zip $D
 rm -fr $D
 
 pkg='linux64'
@@ -27,7 +27,15 @@ mkdir $D
 cp -a /lib/liblove.so* $D
 cat /bin/love loof.love > $D/loof
 chmod +x $D/loof
-zip -9r $D.zip $D
+zip -9ry $D.zip $D
 rm -fr $D
+
+pkg='osx'
+
+D="loof-$pkg"
+find Loof.app -iname "*.sw?" -exec rm {} \;
+cp Info.plist Loof.app/Contents/Info.plist
+cp loof.love Loof.app/Contents/Resources/Loof.love
+zip -9ry $D.zip Loof.app
 
 scp *.zip planet:www/vrac/love/

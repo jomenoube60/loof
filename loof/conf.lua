96c40e79a456e04608410d64338eddd0c62a7b4b
fab <fdev31@gmail.com>
2017-02-27 16:29:23 +0100

    add missing conf.lua

---
+++ loof/conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.window.title = "Loof!"
+    t.window.width = 0
+    t.window.height = 0
+    t.window.vsync = true
+    t.window.fullscreen = true
+    t.window.fullscreentype = "exclusive"
+    t.window.fullscreentype = "exclusive"
+    t.modules.joystick = true
+    t.modules.physics = true
+end
